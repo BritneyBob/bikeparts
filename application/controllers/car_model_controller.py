1bf486d19e866fde2aa965d96bf890c7fd0cd5d5
Britta Carlsson <britta.carlsson@studerande.plushogskolan.se>
2021-12-03 11:34:46 +0100

    added car_model_spare_part data. Working with company_address

--- application/controllers/car_model_controller.py
+++ application/controllers/car_model_controller.py
@@ -3,3 +3,7 @@ from application.data.repository import car_model_repository
 
 def create_car_model(car_model):
     car_model_repository.create_car_model(car_model)
+
+
+def create_car_model_spare_part(car_model, spare_part):
+    car_model_repository.create_car_model_spare_part(car_model, spare_part)
--- application/data/repository/company_repository.py
+++ application/data/repository/company_repository.py
@@ -1,8 +1,14 @@
 from application.data.db import session
-from application.data.models import Company
+from application.data.models import Company, companies_has_addresses_table
 
 
 def create_company(company):
     company = Company(**company)
     session.add(company)
+    session.commit()
+
+
+def create_company_address(company, address):
+    statement = companies_has_addresses_table.insert().values(company_id=company, address_id=address)
+    session.execute(statement)
     session.commit()
