40c72f6f344aed009dbb5535f6fe9b41f154d954
Ekajsa <kajsa.einarsson@studerande.plushogskolan.se>
2021-12-05 11:53:29 +0100

    Before merging from master

--- application/data/repository/spare_part_repository.py
+++ application/data/repository/spare_part_repository.py
@@ -36,10 +36,8 @@ def get_spare_part_by_id(_id):
 
 
 def get_spare_part_supplier_by_id(_id):
-    # TODO Question: Can this one go here?
     return session.query(SparePartSupplier).filter(SparePartSupplier.product_number == _id).all()
 
 
 def get_spare_parts_by_filter(_filter):
-    # TODO How to filter on something that is not exact match? Can't get "like" to work
-    return session.query(SparePart).filter(SparePart.name == _filter).all()
+    return session.query(SparePart).filter(SparePart.name.like(f'%{_filter}%')).all()
