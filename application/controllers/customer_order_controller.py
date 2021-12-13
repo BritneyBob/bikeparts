d8fff2f54a6ffa01a95f28f8b4939bd3faa4e91f
Britta Carlsson <britta.carlsson@studerande.plushogskolan.se>
2021-12-13 15:43:47 +0100

    Did some clean up in customer menu

--- application/controllers/customer_order_controller.py
+++ application/controllers/customer_order_controller.py
@@ -25,30 +25,6 @@ def get_customer_orders_by_order_number(order_number):
     return customer_order_repository.get_customer_orders_by_order_number(order_number)
 
 
-def update_product(order_details, new_product_number):
-    customer_order_repository.update_product(order_details, new_product_number)
-
-
-def update_price(order_details, new_price):
-    customer_order_repository.update_price(order_details, new_price)
-
-
-def update_quantity(order_details, new_quantity):
-    customer_order_repository.update_quantity(order_details, new_quantity)
-
-
-def update_customer(order, new_customer_id):
-    customer_order_repository.update_customer(order, new_customer_id)
-
-
-def update_store(order, new_store_id):
-    customer_order_repository.update_store(order, new_store_id)
-
-
-def update_employee(order, new_employee_id):
-    customer_order_repository.update_employee(order, new_employee_id)
-
-
 def add_shipped_date(order, shipped_date):
     customer_order_repository.add_shipped_date(order, shipped_date)
 
