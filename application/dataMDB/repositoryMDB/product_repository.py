ef4f69dcc0b549f4ca5a127c4cb8b7f95877a174
Ekajsa <kajsa.einarsson@studerande.plushogskolan.se>
2021-12-15 20:09:54 +0100

    product view choice 1 done

--- application/dataMDB/repositoryMDB/product_repository.py
+++ application/dataMDB/repositoryMDB/product_repository.py
@@ -1,68 +1,59 @@
 from application.dataMDB.modelsMDB import Product
 
 
-def create_spare_part(spare_part):
-    spare_part = SparePart(**spare_part)
-    session.add(spare_part)
-    session.commit()
+def create_product(product):
+    Product(product).save()
 
 
-def create_spare_part_supplier(spare_part_supplier):
-    spare_part_supplier = SparePartSupplier(**spare_part_supplier)
-    session.add(spare_part_supplier)
-    session.commit()
+def create_product_supplier(company, product):
+    company.supplies_products.append(product)
+    company.save()
 
 
-def create_spare_part_in_store(spare_part_in_store):
-    spare_part_in_store = SparePartInStore(**spare_part_in_store)
-    session.add(spare_part_in_store)
-    session.commit()
+def create_product_in_store(store, product):
+    store.products.append(product)
+    store.save()
 
 
-def create_spare_part_manufacturer(spare_part, manufacturer):
-    statement = spare_parts_have_manufacturers_table.insert().values(product_number=spare_part,
-                                                                     manufacturer_id=manufacturer)
-    session.execute(statement)
-    session.commit()
+def create_product_manufacturer(company, product):
+    company.manufactures_products.append(product)
+    company.save()
 
 
-def get_spare_parts():
+def get_products():
     return Product.all()
 
 
-def get_spare_parts_by_filter(name_filter):
-    return session.query(SparePart).filter(SparePart.name.like(f'%{name_filter}%')).all()
+def get_products_by_filter(name_filter):
+    return Product.find(name=name_filter).all()
 
 
-def get_spare_part_by_id(product_id):
+def get_product_by_id(product_id):
     return Product.find(_id=product_id).first_or_none()
 
 
-# returns a bunch of supplier id's based on the selected product
-def get_spare_part_suppliers(product_no):
-    return session.query(SparePartSupplier).filter(SparePartSupplier.product_number == product_no).all()
-
-
-# returns company related to supplier_id
-def get_spare_part_supplier_company(supplier_id):
-    return session.query(Company).join(Supplier).filter(Supplier.supplier_id == supplier_id).all()
-
-
-# returns company related to manufacturer_id
-def get_spare_part_manufacturer_company(manufacturer_id):
-    return session.query(Company).join(Manufacturer).filter(Manufacturer.manufacturer_id == manufacturer_id).all()
-
-
-def update_product(product_no, new_name, new_description):
-    product = get_spare_part_by_id(product_no)
+def update_product(product, new_name, new_description):
     product.name = new_name
     product.description = new_description
-    session.add(product)
-    session.commit()
+    product.save()
 
-
-def adjust_price(product_no, new_price):
-    product = session.query(SparePart).filter(SparePart.product_number == product_no).first()
-    product.sell_price = new_price
-    print()
-    session.commit()
+# def adjust_price(product_no, new_price):
+#     product = session.query(SparePart).filter(SparePart.product_number == product_no).first()
+#     product.sell_price = new_price
+#     print()
+#     session.commit()
+#
+#
+# # returns a bunch of supplier companies's based on the selected product
+# def get_product_suppliers(product_no):
+#     pass
+#
+# # returns company related to supplier_id
+# def get_spare_part_supplier_company(supplier_id):
+#     return session.query(Company).join(Supplier).filter(Supplier.supplier_id == supplier_id).all()
+#
+#
+# # returns company related to manufacturer_id
+# def get_spare_part_manufacturer_company(manufacturer_id):
+#     return session.query(Company).join(Manufacturer).filter(Manufacturer.manufacturer_id == manufacturer_id).all()
+#
--- application/view_mdb/products.py
+++ application/view_mdb/products.py
@@ -1,65 +1,66 @@
-from application.controllers import spare_part_controller, company_controller
-from application.controllers.spare_part_controller import adjust_price, update_product
-from application.controllers.store_controller import view_stores, get_spare_part_in_store_by_store_id_and_product_number
+from application.controllersMDB import product_controller as prc, company_controller as cc, store_controller as stc
 
 
-def search_spare_parts():
+def search_products():
     name_filter = input("Which product are you looking for? Enter name/part of name. Leave blank to show all: ")
-    search_hits = spare_part_controller.get_spare_parts_by_filter(name_filter)
-    for spare_part in search_hits:
-        print(f"Product number: {spare_part.product_number}\t\tProduct name: {spare_part.name}\t\t"
-              f"Description: {spare_part.description}\t\tPrice to customer: {spare_part.sell_price} EUR")
+    products = prc.get_products()
+    for product in products:
+        if name_filter.lower() in str(product.name).lower():
+            print(f"Product number: {product.product_number}\t\tProduct name: {product.name}\t\t"
+                  f"Description: {product.description}\t\tPrice to customer: {product.sell_price} EUR")
+        else:
+            continue
 
 
-def show_all_spare_parts():
+def show_all_products():
     print("List of available spare parts: ")
-    spare_parts = spare_part_controller.get_spare_parts()
+    spare_parts = prc.get_products()
     for spare_part in spare_parts:
         print(f"Product number: {spare_part.product_number}\t\tName: {spare_part.name}"
               f"\t\tDescription: {spare_part.description}\t\tPrice: {spare_part.sell_price} EUR")
 
 
-def show_one_spare_part():
+def show_one_product():
     product_no = input("Enter product number to show one product: ")  # TODO Error handling of input!
     print("*" * 50)
-    print_spare_part_info(product_no)
+    print_product_info(product_no)
     print_supplier(product_no)
     print_manufacturer(product_no)
     print_stock_info(product_no)
     print("*" * 50)
 
 
-def print_spare_part_info(product_no):
-    spare_part = spare_part_controller.get_spare_part_by_id(product_no)
+def print_product_info(product_no):
+    spare_part = prc.get_product_by_id(product_no)
     print(f"Product number and name: {spare_part.product_number} {spare_part.name}")
     print(f"Description: {spare_part.description}")
     print(f"Customer price: {spare_part.sell_price} EUR")
 
 
 def print_supplier(product_no):
-    suppliers = spare_part_controller.get_spare_part_suppliers(product_no)
+    suppliers = prc.get_spare_part_suppliers(product_no)
     print(f"Available from the following suppliers: ")
     for supplier in suppliers:
-        supplier_company = spare_part_controller.get_spare_part_supplier_company(supplier.supplier_id)
+        supplier_company = prc.get_spare_part_supplier_company(supplier.supplier_id)
         for company in supplier_company:
             print(f"\tCompany name: {company.company_name}\tBuy price: {supplier.buy_price} EUR")
 
 
 def print_manufacturer(product_no):
-    spare_part = spare_part_controller.get_spare_part_by_id(product_no)
+    spare_part = prc.get_product_by_id(product_no)
     manufacturer_id_s = [manufacturer.manufacturer_id for manufacturer in spare_part.manufacturers]
     print(f"Manufactured by: ")
     for manufacturer_id in manufacturer_id_s:
-        manufacturer_company = spare_part_controller.get_spare_part_manufacturer_company(manufacturer_id)
+        manufacturer_company = prc.get_spare_part_manufacturer_company(manufacturer_id)
         for company in manufacturer_company:
             print(f"\tCompany name: {company.company_name}")
 
 
 def print_stock_info(product_no):
-    sorted_stores = sorted(view_stores(), key=lambda x: x.store_id)
+    sorted_stores = sorted(stc.get_stores(), key=lambda x: x.store_id)
     print(f"Items in stock in the following stores: ")
     for store in sorted_stores:
-        product_in_stock = get_spare_part_in_store_by_store_id_and_product_number(store.store_id, product_no)
+        product_in_stock = stc.get_spare_part_in_store_by_store_id_and_product_number(store.store_id, product_no)
         if product_in_stock is None:
             continue
         else:
@@ -68,11 +69,11 @@ def print_stock_info(product_no):
 
 
 def update_a_product():
-    search_spare_parts()
+    search_products()
     product_no = input("\nEnter product number to update: ")
-    spare_part = spare_part_controller.get_spare_part_by_id(product_no)
+    spare_part = prc.get_product_by_id(product_no)
     print("*" * 50)
-    print_spare_part_info(product_no)
+    print_product_info(product_no)
     print("*" * 50)
 
     # TODO Error handling of inputs that are too long for the database field
@@ -92,11 +93,11 @@ def update_a_product():
             new_description = new_description
             # except (DataError, errors.DataError):
             #     print("You have entered a too long description, please try again!")
-        update_product(product_no, new_name, new_description)
+        prc.update_product(product_no, new_name, new_description)
 
         print("\nProduct info was updated. New information is:")
         print("*" * 50)
-        print_spare_part_info(product_no)
+        print_product_info(product_no)
         print("*" * 50)
 
 
@@ -110,14 +111,14 @@ def add_product():
         "description": description,
         "sell_price": sell_price
     }
-    spare_part_controller.create_spare_part(new_product)
+    prc.create_product(new_product)
     adding_info = True
-    added_product = spare_part_controller.get_spare_parts_by_filter(name)
+    added_product = prc.get_products_by_filter(name)
     for product in added_product:
         # TODO what if there is a double? Is there a way to get the recently added product no?
         while adding_info:
             print("Product details for the new product: ")
-            print_spare_part_info(product.product_number)
+            print_product_info(product.product_number)
             print_supplier(product.product_number)
             print_manufacturer(product.product_number)
             print_stock_info(product.product_number)
@@ -142,19 +143,21 @@ def add_supplier_to_product(product):
     adding_suppliers = True
     while adding_suppliers:
         print("Available suppliers: ")
-        all_suppliers = company_controller.get_suppliers()
-        for supplier in all_suppliers:
-            print(f"Id: {supplier.supplier_id} Company name: {supplier.company.company_name}")
+        all_companies = cc.get_all_companies()
+        for company in all_companies:
+            if len(company.supplies_products) > 0:
+                print(f"Id: {company.supplier_id} Company name: {company.company.company_name}")
+            else:
+                continue
         chosen_supplier_id = input("Enter a supplier id from the list: ")
         price = input("Enter the agreed buy price (EUR): ")
         delivery_time = input("Enter the number of days to deliver from this supplier: ")
-        spare_part_supplier = {
-            "product_number": product.product_number,
+
+        product_supplier = {
             "supplier_id": chosen_supplier_id,
-            "buy_price": price,
-            "delivery_time": delivery_time
+            "new_company_id": cc.get_company_by_id()
         }
-        spare_part_controller.create_spare_part_supplier(spare_part_supplier)
+        prc.create_product_supplier(product_supplier, product)
         add_more = input("Do you want to add another supplier? Y/N: ")
         if add_more.lower() == "n":
             adding_suppliers = False
@@ -164,11 +167,11 @@ def add_manufacturer_to_product(product):
     adding_manufacturers = True
     while adding_manufacturers:
         print("Available manufacturers: ")
-        all_manufacturers = company_controller.get_manufacturers()
+        all_manufacturers = cc.get_all_companies()
         for manufacturer in all_manufacturers:
             print(f"Id: {manufacturer.manufacturer_id} Company name: {manufacturer.company.company_name}")
         chosen_manufacturer_id = input("Enter a manufacturer id from the list: ")
-        spare_part_controller.create_spare_part_manufacturer(product.product_number, chosen_manufacturer_id)
+        prc.create_product_manufacturer(product.product_number, chosen_manufacturer_id)
         add_more = input("Do you want to add another manufacturer? Y/N: ")
         if add_more.lower() == "n":
             adding_manufacturers = False
@@ -178,7 +181,7 @@ def add_stores_to_product(product):
     adding_stores = True
     while adding_stores:
         print("Available manufacturers: ")
-        all_stores = view_stores()
+        all_stores = stc.get_stores()
         for store in all_stores:
             print(f"Id: {store.store_id} City: {store.address.city_name}")
         chosen_store_id = input("Enter a store id from the list: ")
@@ -194,19 +197,19 @@ def add_stores_to_product(product):
             "lowest_index": lowest_index,
             "quantity_to_order": quantity_to_order
         }
-        spare_part_controller.create_spare_part_in_store(spare_part_in_store)
+        prc.create_product_in_store(store, spare_part_in_store)
         add_more = input("Do you want to add the product to another store? Y/N: ")
         if add_more.lower() == "n":
             adding_stores = False
 
 
-def adjust_sell_margins():
-    name_filter = input("Which products sell margin do you want to adjust? Enter name/part of name: ")
-    search_hits = spare_part_controller.get_spare_parts_by_filter(name_filter)
-    for hit in search_hits:
-        print(hit)
-    product_no = input('\nEnter Product number for the product you want to adjust sell margins on: ')
-    print_spare_part_info(product_no)
-    new_price = input('Enter new sell price, EUR: ')
-    adjust_price(product_no, new_price)
-    print('Sell price has been updated.')
+# def adjust_sell_margins():
+#     name_filter = input("Which products sell margin do you want to adjust? Enter name/part of name: ")
+#     search_hits = prc.get_products_by_filter(name_filter)
+#     for hit in search_hits:
+#         print(hit)
+#     product_no = input('\nEnter Product number for the product you want to adjust sell margins on: ')
+#     print_spare_part_info(product_no)
+#     new_price = input('Enter new sell price, EUR: ')
+#     stc.adjust_price(product_no, new_price)
+#     print('Sell price has been updated.')
